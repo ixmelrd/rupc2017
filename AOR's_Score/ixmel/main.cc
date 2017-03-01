8923789ac49d38827cffddbd66f065b99a0390dc
ixmel <kazuyakazuya524@gmail.com>
2017-03-01 16:21:45 +0900

    add ixmel solution

--- AOR's_Score/ixmel/main.cc
+++ AOR's_Score/ixmel/main.cc
@@ -1,41 +1,14 @@
 #include<iostream>
 #include<vector>
-#include<string>
-#include<algorithm>	
-#include<map>
-#include<set>
-#include<utility>
-#include<cmath>
-#include<cstring>
 #include<queue>
-#include<stack>
-#include<cstdio>
-#include<sstream>
-#include<iomanip>
 #define loop(i,a,b) for(int i=a;i<b;i++) 
 #define rep(i,a) loop(i,0,a)
 #define pb push_back
-#define mp make_pair
-#define all(in) in.begin(),in.end()
-#define shosu(x) fixed<<setprecision(x)
 using namespace std;
-//kaewasuretyuui
-typedef long long ll;
 typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef vector<pii> vp;
-typedef vector<vp> vvp;
-typedef vector<string> vs;
-typedef vector<double> vd;
-typedef vector<vd> vvd;
-typedef pair<int,pii> pip;
-typedef vector<pip>vip;
-const double PI=acos(-1);
-const double EPS=1e-9;
 const int inf=1e8;
-int dx[4]={0,1,0,-1};
-int dy[4]={1,0,-1,0};
 struct edge{
 	int to,cap,cost,rev;
 };
---
+++ Jump_Party/ixmel/main.cc
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#define loop(i,a,b) for(int i=a;i<b;i++) 
+#define rep(i,a) loop(i,0,a)
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+int main(){
+	int n,m,q;
+	cin>>n>>m>>q;
+	n*=m;
+	vvi in(18,vi(n));
+	rep(i,n){
+		int a,b;
+		cin>>a>>b;
+		in[0][i]=a*m+b;
+	}
+	vi hu(q);
+	rep(i,q){
+		int a,b;
+		cin>>a>>b;
+		hu[i]=a*m+b;
+	}
+	rep(i,17)rep(j,n)in[i+1][j]=in[i][in[i][j]];
+
+	int l=0,r=n;
+	while(r-l>1){
+		int h=(l+r)/2;
+
+		vi now(n);
+		rep(i,n)now[i]=i;
+		rep(i,18)if(h&1<<i)rep(j,n)now[j]=in[i][now[j]];
+
+		vi H(n);
+		rep(i,q)H[now[hu[i]]]++;
+
+		bool han=true;
+		rep(i,n)if(H[i]>1)han=false;
+
+		if(han)l=h;
+		else r=h;
+	}
+	if(r==n)r=-1;
+	cout<<r<<endl;
+}
+
+
+
+
+
+
+
+
+
+

0e3a03541ba299634e1c574e955f373042b475ee
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2017-03-03 13:58:31 +0900

    T.Mがgenerator.cを作成

--- Password/tests/constraints.hpp
+++ Password/tests/constraints.hpp
@@ -2,3 +2,6 @@
 #define MAX_A 1000
 #define MIN_B 1
 #define MAX_B 1000
+#define MIN_N 1
+#define MAX_N 100000
+#define MAX_SUM_LEN 100000
---
+++ Password/tests/generator.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include "./testlib.h"
+#include "./constraints.hpp"
+#include <sys/types.h>
+#include <unistd.h>
+
+void makeNstring(int n,char *name){
+  FILE *f;
+  int i,l,use=0;
+  f=fopen(name,"w");
+  fprintf(f,"%d\n",n);
+  for(i=0;i<n;i++){
+    l=rnd.next(1,MAX_SUM_LEN-use-(n-i-1));
+    use+=l;
+    while(l--)fprintf(f,"%c",rnd.next(0,25)+'a');
+    fprintf(f,"\n");
+  }//printf("%d\n",use);
+  fclose(f);
+}
+void charange00(){
+  FILE *f;
+  int i;
+  f=fopen("60_Charange_00.in","w");
+  fprintf(f,"1\na");
+  for(i=1;i<26*26*26;i++)fprintf(f,"a%c%c%c",i/26/26+'a',i/26%26+'a',i%26+'a');
+  fprintf(f,"\n");
+  fclose(f);
+}
+// aとbをファイルストリームに出力する
+// ファイル名は prefix_num.in (ex: 00_sample_00.in)
+
+
+int main(){
+    // 乱数のシードを設定
+    // pidを足すことで、1秒以上間を置かずに起動したときに同じシードになってしまうのを防ぐ
+  rnd.setSeed(time(0)+getpid());//[]
+  int i;
+  char s[100];
+  for(i=0;i<10;i++){
+    int n=rnd.next(MIN_N,MAX_N);
+    sprintf(s,"50_random_%02d.in",i);
+    makeNstring(n,s);
+  }
+  charange00();
+  return 0;
+}

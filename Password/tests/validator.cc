b9edec8bf3d9858f8aae7d37379a5d75732ebe67
野井 <noy@harsan.local>
2017-03-03 15:59:39 +0900

    add validator

--- Password/tests/validator.cc
+++ Password/tests/validator.cc
@@ -1,12 +1,24 @@
+#include <iostream>
+#include <string>
+#include <ctype.h>
 #include "./testlib.h"
 #include "./constraints.hpp"
 #include <cassert>
+using namespace std;
 
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
-    inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+    int n = inf.readInt(MIN_N, MAX_N);
     inf.readEoln();
+
+    int string_length_sum = 0;
+    string regex = "[a-z]*";
+
+    for(int i = 0; i < n; i++){
+        string s = inf.readToken(regex);
+        inf.readEoln();
+        string_length_sum += s.size();
+    }
+    assert(string_length_sum <= MAX_SUM_LEN);
     inf.readEof();
 }

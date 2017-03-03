95518f8dc7bec30040f6d3a995a70f149d1806cf
TsutsumiShogo <threepipesatos@gmail.com>
2017-03-03 21:31:07 +0900

    generatorの修正(ランダムケース10個)

--- Energy_Drink/tests/generator.cc
+++ Energy_Drink/tests/generator.cc
@@ -7,15 +7,19 @@
 
 using namespace std;
 
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
+int a[MAX_N];
+int b[MAX_N];
 
 // aとbをファイルストリームに出力する
 // ファイル名は prefix_num.in (ex: 00_sample_00.in)
-void output(int a, int b, const string &prefix, int num){
+void output(int n, const string &prefix, int num){
     char name[100];
     sprintf(name, "%s_%02d.in", prefix.c_str(), num);
     ofstream ofs(name);
-    ofs << a << " " << b << endl;
+    ofs << n << endl;
+    for(int i = 0; i < n; ++i){
+        ofs << a[i] << " " << b[i] << endl;
+    }
     ofs.close();
 }
 
@@ -26,20 +30,11 @@ int main(){
 
     // 乱数ケースを10個生成
     for(int i = 0; i < 10; ++i){
-        int A = rnd.next(MIN_A, MAX_A);
-        int B = rnd.next(MIN_B, MAX_B);
-        output(A, B, "50_random", i);
-    }
-
-    // 片方が大きいケースを生成
-    for(int i = 0; i < 10; ++i){
-        int A = 1;
-        int B = 1;
-        while(0.5*A <= B && B <= 1.5*A){
-            A = rnd.next(MIN_A, MAX_A);
-            B = rnd.next(MIN_B, MAX_B);
+        int N = rnd.next(MIN_N, MAX_N);
+        for(int j = 0; j < N; ++j){
+            a[j] = rnd.next(MIN_A, MAX_A);
+            b[j] = rnd.next(MIN_B, MAX_B);
         }
-        if(rnd.next(0,1)) swap(A, B);
-        output(A, B, "60_unbalance", i);
+        output(N, "50_random", i);
     }
 }

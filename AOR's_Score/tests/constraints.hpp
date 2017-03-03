49cdf5aaa9491df7889bd0aa4d4c88576cae66b5
TsutsumiShogo <threepipesatos@gmail.com>
2017-03-03 23:59:22 +0900

    fix constraints of validator

--- AOR's_Score/tests/constraints.hpp
+++ AOR's_Score/tests/constraints.hpp
@@ -1,13 +1,13 @@
 #define MIN_N 1
 #define MAX_N 1000
-#define MIN_M 1
+#define MIN_M 0
 #define MAX_M 1000
-#define MIN_K 1
-#define MIN_A 1
+#define MIN_K 0
+#define MIN_A 0
 #define MAX_A 1000
-#define MIN_B 1
+#define MIN_B 0
 #define MAX_B 1000
-#define MIN_T 1
+#define MIN_T 0
 
 /*
 MIN_K, MIN_T はN依存のため書いていません

064393b09ce0a9e281c1183d49cbababd9f71a21
Yazaten <Yazaten@gmail.com>
2017-03-04 00:23:10 +0900

    AORイカちゃんの成績のconstraintsを修正した

--- AOR's_Score/tests/constraints.hpp
+++ AOR's_Score/tests/constraints.hpp
@@ -1,15 +1,15 @@
 #define MIN_N 1
-#define MAX_N 1000
+#define MAX_N 100
 #define MIN_M 0
-#define MAX_M 1000
+#define MAX_M 100
 #define MIN_K 0
 #define MIN_A 0
-#define MAX_A 1000
+#define MAX_A 100
 #define MIN_B 0
-#define MAX_B 1000
+#define MAX_B 100
 #define MIN_T 0
 
 /*
-MIN_K, MIN_T はN依存のため書いていません
+MAX_K, MAX_T はN依存のため書いていません
 必要なら追加してください
 */

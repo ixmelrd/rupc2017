21c3971ce71f1e0bdedc5adfd92f3e3a7eb5175a
TsutsumiShogo <threepipesatos@gmail.com>
2017-03-03 23:44:28 +0900

    modified validator.cc of AOR's_Score

--- AOR's_Score/tests/constraints.hpp
+++ AOR's_Score/tests/constraints.hpp
@@ -1,4 +1,15 @@
+#define MIN_N 1
+#define MAX_N 1000
+#define MIN_M 1
+#define MAX_M 1000
+#define MIN_K 1
 #define MIN_A 1
 #define MAX_A 1000
 #define MIN_B 1
 #define MAX_B 1000
+#define MIN_T 1
+
+/*
+MIN_K, MIN_T はN依存のため書いていません
+必要なら追加してください
+*/
--- AOR's_Score/tests/validator.cc
+++ AOR's_Score/tests/validator.cc
@@ -2,11 +2,34 @@
 #include "./constraints.hpp"
 #include <cassert>
 
+void readIntLine(int len, int min, int max){
+    for(int i = 0; i < len; ++i){
+        if(i > 0) inf.readSpace();
+        inf.readInt(min, max);
+    }
+    inf.readEoln();
+}
+
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
+    // read first line
+    int N = inf.readInt(MIN_N, MAX_N);
     inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+    int M = inf.readInt(MIN_M, MAX_M);
+    inf.readSpace();
+    inf.readInt(MIN_K, N);
     inf.readEoln();
+
+    // read second line
+    readIntLine(N, MIN_A, MAX_A);
+
+    // read b
+    for(int i = 0; i < M; ++i){
+        readIntLine(N, MIN_B, MAX_B);
+    }
+
+    // read last line
+    readIntLine(M, MIN_T, N);
+    
     inf.readEof();
 }

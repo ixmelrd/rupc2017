e4a015fa26b54620bca9fcb401bf2d360f51b441
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2017-03-05 14:36:08 +0900

    Jump-PartyにT.MのTLE解を追加

---
+++ Jump_Party/TM-TLE/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+  int h,w,p[30010],d[30010],i,j,n,x,y;
+  scanf("%d %d %d",&h,&w,&n);
+  for(i=0;i<h*w;i++){
+    scanf("%d %d",&x,&y);
+    d[i]=y*w+x;
+  }
+  for(i=0;i<n;i++){
+    scanf("%d %d",&x,&y);
+    p[i]=y*w+x;
+  }
+  for(i=0;i<h*w+10;i++){
+    int f[30010]={};
+    for(j=0;j<n;j++){
+      p[j]=d[p[j]];
+      if(f[p[j]])goto END;
+      f[p[j]]=1;
+    }
+  }
+ END:;
+  printf("%d\n",i-h*w-10?i+1:-1);
+  return 0;
+}

50480b72b90971b057271a8a53f84c116b54ca25
tubo028 <arsenic28@gmail.com>
2017-03-11 19:10:41 +0900

    Jump Partyにvalidatorを追加

--- Jump_Party/tests/validator.cc
+++ Jump_Party/tests/validator.cc
@@ -4,9 +4,29 @@
 
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
+    int H = inf.readInt(MIN_H, MAX_H);
     inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+    int W = inf.readInt(MIN_W, MAX_W);
+    inf.readSpace();
+    int N = inf.readInt(MIN_N, MAX_N);
     inf.readEoln();
-    inf.readEof();
+
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+            inf.readInt(0, H - 1);
+            inf.readSpace();
+            inf.readInt(0, W - 1);
+            if (j == W - 1) inf.readEoln();
+            else inf.readSpace();
+        }
+    }
+
+    for (int i = 0; i < N; ++i) {
+        inf.readInt(0, H - 1);
+        inf.readSpace();
+        inf.readInt(0, W - 1);
+        inf.readEoln();
+    }
+
+    inf. readEof();
 }

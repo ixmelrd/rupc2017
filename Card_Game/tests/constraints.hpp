38a5e1bb51dd4ba84d7f2c09a0b966d8e6dcd9f7
野井 <noy@harsan.local>
2017-03-11 12:17:59 +0900

    add validator.cc

--- Card_Game/tests/constraints.hpp
+++ Card_Game/tests/constraints.hpp
@@ -1,10 +1,14 @@
-#define MIN_A 1
-#define MAX_A 1000
-#define MIN_B 1
-#define MAX_B 1000
-
-#define MIN_H 1
-#define MAX_H 500
-#define MIN_W 1
-#define MAX_W 500
 #define MIN_N 1
+#define MAX_N 100000
+#define MIN_M 2
+#define MAX_M 200000
+#define MIN_R 2
+#define MAX_R 10
+#define MIN_Q 1
+#define MAX_Q 100000
+#define MIN_a_b 1
+#define MIN_c 0
+#define MAX_c 100000
+#define MIN_x_y 1
+#define MIN_z 0
+#define MAX_z 100000
--- Card_Game/tests/validator.cc
+++ Card_Game/tests/validator.cc
@@ -4,9 +4,35 @@
 
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
+
+    int n = inf.readInt(MIN_N, MAX_N);
     inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+    int m = inf.readInt(MIN_M, MAX_M);
+    inf.readSpace();
+    inf.readInt(MIN_R, MAX_R);
+    inf.readSpace();
+    int q = inf.readInt(MIN_Q, MAX_Q);
     inf.readEoln();
+
+    for(int i = 0; i < m; i++){
+        int a = inf.readInt(MIN_a_b, n);
+        inf.readSpace();
+        int b = inf.readInt(MIN_a_b, n);
+        inf.readSpace();
+        inf.readInt(MIN_c, MAX_c);
+        inf.readEoln();
+
+        assert(a != b);
+    }
+
+    for(int i = 0; i < q; i++){
+        inf.readInt(MIN_x_y, n);
+        inf.readSpace();
+        inf.readInt(MIN_x_y, n);
+        inf.readSpace();
+        inf.readInt(MIN_z, MAX_z);
+        inf.readEoln();
+    }
+
     inf.readEof();
 }

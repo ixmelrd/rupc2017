56df132b058a4b489badb3269c010f1f3277938b
Yazaten <Yazaten@gmail.com>
2017-03-13 12:16:39 +0900

    Range_Numにgeneratorを追加した

--- Range_Num/tests/generator.cc
+++ Range_Num/tests/generator.cc
@@ -4,6 +4,7 @@
 #include "./constraints.hpp"
 #include <sys/types.h>
 #include <unistd.h>
+#include <cassert>
 
 using namespace std;
 
@@ -33,18 +34,48 @@ int main(){
     // pidを足すことで、1秒以上間を置かずに起動したときに同じシードになってしまうのを防ぐ
     rnd.setSeed(time(0)+getpid());
 
-    // 小さい乱数ケース
-    for(int i = 0; i < 10; ++i){
-        int N = rnd.next(MIN_N, 10);
-        int M = rnd.next(MIN_M, N);
-        vector<int> A = gen(N);
-        output(N, M, A, "50_random", i);
-    }
-    // 大きい乱数ケース
+    // 乱数ケース
     for(int i = 0; i < 10; ++i){
         int N = rnd.next(MIN_N, MAX_N);
         int M = rnd.next(MIN_M, N);
         vector<int> A = gen(N);
         output(N, M, A, "50_random", i);
     }
+
+    // 色の数Nが少ない乱数ケース
+    for(int i = 0; i < 10; ++i){
+        int N = rnd.next(MIN_N, 10);
+        int M = rnd.next(MIN_M, N);
+        vector<int> A = gen(N);
+        output(N, M, A, "51_random_Nsmall", i);
+    }
+
+    // 最大( LCM(N,M)が大きく、数の範囲の総和も大きい )ケース
+    vector<int> primenums;
+    const auto isPrime = [](int n){
+        if(n<=1)return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0)return false;
+        }
+        return true;
+    };
+    for(int i=900;i<=1000;i++){
+        if(isPrime(i)){
+            primenums.push_back(i);
+        }
+    }
+
+    for(int i = 0; i < 10; ++i){
+        int pos = rnd.next(1,(int)primenums.size()-1);
+        int N = primenums[ pos ];
+        int M = primenums[ rnd.next(0,pos-1) ];
+        assert(M<=N);
+        vector<int> A(N);
+        for(int j=0;j<N;j++){
+            if(j%2==0)A[j]=0;
+            else A[j]=100;
+        }
+        output(N, M, A, "52_NMprime", i);
+    }
+
 }

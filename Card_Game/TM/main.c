b1e2450f98dbd18269241d2a82ccae7d0c89aabf
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2017-03-13 13:34:45 +0900

    Card_GameのT.Mの回答を修正

--- Card_Game/TM/main.c
+++ Card_Game/TM/main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-long long R=1,C=1,H[2000010],N[2000010],s=0,d[20][100010],min;
+long long R=1,C=1,H[2000010],N[2000010],s=0,d[20][100110],min;
 //評価関数（いまはMIN）
 int hyouka(int a,int b){
   if(C<b)return 1;
@@ -19,11 +19,11 @@ int hout(){
   H[j/2]=k;
   return rt;
 }
-long long CO[100010];
+long long CO[100110];
 void dijk(int v,int e,int *fr,int *to,int *co,int mi){
-  int i,id[100010],ta[100010],nt[100010],f[100010]={0};
+  int i,id[100110],ta[100110],nt[300110],f[100110]={0};
   for(i=0;i<=v;i++)ta[i]=-1;
-  for(i=0;i<=v;i++)CO[i]=1000000000;
+  for(i=0;i<=v;i++)CO[i]=1e18;
   for(i=CO[mi]=0;i<e;i++){
     nt[i]=ta[fr[i]];
     ta[fr[i]]=i;
@@ -36,7 +36,7 @@ void dijk(int v,int e,int *fr,int *to,int *co,int mi){
     while(f[mi]&&C-1)mi=id[hout()];
   }
 }
-int  a[100110],b[100110],c[100110];
+int  a[300110],b[300110],c[300110];
 int main(){
   int n,m,r,q,x,y,z,i,j;
   scanf("%d %d %d %d",&n,&m,&r,&q);
@@ -47,7 +47,8 @@ int main(){
     c[i+m]=0;
   }
   for(i=0;i<r;i++){
-    dijk(n+r,m+n,a,b,c,(i+1)%r+n+1);
+    R=C=1;
+    dijk(n+r,m+n,a,b,c,i+n+1);
     for(j=1;j<=n;j++)d[i][j]=CO[j];
   }
   for(i=0;i<q;i++){
@@ -57,6 +58,7 @@ int main(){
       if(min>d[j][x]+d[j][y])min=d[j][x]+d[j][y];
     }
     if(min<z)s+=z-min;
+    //s+=z-min;
   }
   printf("%lld\n",s);
   return 0;

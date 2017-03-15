8d89febf7191377701d41d7d0489da02acba2be6
Yazaten <Yazaten@gmail.com>
2017-03-15 15:42:50 +0900

    Change_of_Busesのvalidatorを作成した

--- Change_of_Buses/tests/constraints.hpp
+++ Change_of_Buses/tests/constraints.hpp
@@ -6,3 +6,8 @@
 #define MAX_t 100000
 #define MIN_c 0
 #define MAX_c 100000
+
+#define MIN_S 1
+#define MIN_G 1
+#define MIN_u 1
+#define MIN_v 1
--- Change_of_Buses/tests/validator.cc
+++ Change_of_Buses/tests/validator.cc
@@ -1,12 +1,80 @@
 #include "./testlib.h"
 #include "./constraints.hpp"
 #include <cassert>
+#include <vector>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+typedef pair<int,int> pii;
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;i++)
+#define INF 999999999
+
+struct edge{int to,cost;};
+
+void dijkstra(int s, vector<int> &d, vector<edge> G[MAX_N]){
+    priority_queue< pii,vector<pii>,greater<pii> > que;
+    rep( i,d.size() )d[i]=INF;
+    d[s] = 0;
+    que.push( pii(0,s) );
+    
+    while( que.size() ){
+        pii p=que.top();
+        que.pop();
+        
+        int v=p.second;
+        if(d[v]<p.first)continue;
+        
+        rep(i,G[v].size()){
+            edge e=G[v][i];
+            if(d[e.to]>d[v]+e.cost){
+                d[e.to]=d[v]+e.cost;
+                que.push(pii(d[e.to],e.to));
+            }
+        }
+    }
+}
+
+
+bool isReachable(int v, int s, int g, vector<edge> graph[MAX_M]){
+	vector<int> d(v);
+	dijkstra(s,d,graph);
+
+	if(d[g]<INF)return true;
+	else return false;
+}
 
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
+    int N = inf.readInt(MIN_N,MAX_N);
     inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+    int M = inf.readInt(MIN_M,MAX_M);
+    inf.readSpace();
+    int S = inf.readInt(MIN_S,N);
+    inf.readSpace();
+    int G = inf.readInt(MIN_G,N);
     inf.readEoln();
+
+    assert(S!=G);
+    S--,G--;
+
+    vector<edge> graph[MAX_N];
+    rep(i,M){
+    	int u = inf.readInt(MIN_u,N);
+    	inf.readSpace();
+    	int v = inf.readInt(MIN_v,N);
+    	inf.readSpace();
+    	inf.readInt(MIN_t,MAX_t);
+    	inf.readSpace();
+    	inf.readInt(MIN_c,MAX_c);
+		inf.readEoln();
+    	u--,v--;
+    	graph[u].pb(edge{v,0});
+    }
+
+    assert( isReachable(N, S, G, graph) );
+
     inf.readEof();
 }

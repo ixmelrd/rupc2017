af9652ebd05fcd733ca175e9cc6cc13e82b5bb4b
tubo028 <arsenic28@gmail.com>
2017-03-18 03:41:02 +0900

    雨降りバス停 チャレンジケースを追加

--- Islands_Survival/tests/generator.cc
+++ Islands_Survival/tests/generator.cc
@@ -79,8 +79,34 @@ vector<PR> path_graph(int N) {
     return edges;
 }
 
-vector<PR> dango_graph(int N) {
-    // TODO : 団子状のグラフを付くる
+vector<PR> dango_graph() {
+    const int SUPER_TREE_NODE = 90;
+    const int DENCE_NODE = 50;
+    const int DENCE_EDGE = 1000;
+    assert(DENCE_NODE * SUPER_TREE_NODE <= MAX_N);
+    assert(DENCE_NODE * DENCE_EDGE + (SUPER_TREE_NODE - 1) <= MAX_M);
+
+    vector<PR> edges;
+
+    for (int i = 0; i < SUPER_TREE_NODE; ++i) {
+        vector<PR> dence = add_edges_to_tree(DENCE_NODE, DENCE_EDGE);
+        for (auto &e : dence) {
+            e.first += DENCE_NODE * i;
+            e.second += DENCE_NODE * i;
+        }
+        edges.insert(edges.end(), all(dence));
+    }
+
+    vector<PR> tree = ::tree(SUPER_TREE_NODE);
+    for (auto &e : tree) {
+        int u, v;
+        tie(u, v) = e;
+        u = u*DENCE_NODE + rnd.next(0, DENCE_NODE - 1);
+        v = v*DENCE_NODE + rnd.next(0, DENCE_NODE - 1);
+        edges.emplace_back(u, v);
+    }
+
+    return edges;
 }
 
 vector<TPL> random_t(const vector<PR> &edges, int max_t) {
@@ -111,12 +137,12 @@ int main(){
 
     // 木に適当に辺を追加
     // 手計算で確認用の小さいケース (意外とコーナーケースが生成されたりもする)
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < 10; ++i) {
         int N = rnd.next(MIN_N, 5);
         int M = rnd.next(N - 1, N*(N - 1)/2);
         int T = 5;
         vector<TPL> g = random_t(add_edges_to_tree(N, M), T);
-        output(N, M, T, g, "40_random_small", i);
+        output(N, M, T, g, "50_random_small", i);
     }
 
     for (int i = 0; i < 10; ++i) {
@@ -124,7 +150,7 @@ int main(){
         int M = min(MAX_M, N * (int)rnd.next(1.0, 1.1));
         int T = rnd.next(MIN_T, MAX_T);
         vector<TPL> g = random_t(add_edges_to_tree(N, M), T);
-        output(N, M, T, g, "50_random_sparce", i);
+        output(N, M, T, g, "51_random_sparce", i);
     }
 
     for (int i = 0; i < 10; ++i) {
@@ -132,7 +158,7 @@ int main(){
         int M = rnd.next(N - 1, (int)min((long long)MAX_M, (long long)N*(N - 1)/2));
         int T = rnd.next(MIN_T, MAX_T);
         vector<TPL> g = random_t(add_edges_to_tree(N, M), T);
-        output(N, M, T, g, "50_random_dence", i);
+        output(N, M, T, g, "52_random_dence", i);
     }
 
     // 木
@@ -140,7 +166,7 @@ int main(){
         int N = rnd.next(MIN_N, MAX_N);
         int T = rnd.next(MIN_T, MAX_T);
         vector<TPL> t = random_t(tree(N), T);
-        output(N, N - 1, T, t, "50_tree", i);
+        output(N, N - 1, T, t, "53_tree", i);
     }
 
     // スター
@@ -148,14 +174,23 @@ int main(){
         int N = rnd.next(MIN_N, MAX_N);
         int T = rnd.next(MIN_T, MAX_T);
         vector<TPL> t = random_t(star(N), T);
-        output(N, N - 1, T, t, "50_star", i);
+        output(N, N - 1, T, t, "54_star", i);
+    }
+
+    // 大きい密な成分がたくさん
+    for (int i = 0; i < 4; ++i) {
+        int N = 4500;
+        vector<TPL> g = random_t(dango_graph(), MAX_T);
+        int M = g.size();
+        int T = MAX_T;
+        output(N, M, T, g, "55_dange", i);
     }
 
     // 長いパス (DFS殺し)
     {
         int N = MAX_N;
         vector<TPL> g = random_t(path_graph(N), MAX_T);
-        output(N, N - 1, MAX_T, g, "50_dfs_killer", 0);
+        output(N, N - 1, MAX_T, g, "56_dfs_killer", 0);
     }
 
     // オーバーフロー殺し
@@ -163,7 +198,7 @@ int main(){
         int N = MAX_N;
         int M = MAX_M;
         vector<TPL> g = max_t(add_edges_to_tree(N, M));
-        output(N, M, MAX_T, g, "50_32bit_killer", 0);
+        output(N, M, MAX_T, g, "57_32bit_killer", 0);
     }
     
 }

faea7081f290b1d8687b0b49572b96234b510547
kazuya kishida <kazuya kishida>
2017-03-17 16:05:25 +0900

    いらないテンプレ削除

--- Graduation_Ceremony/ixmel/main.cc
+++ Graduation_Ceremony/ixmel/main.cc
@@ -1,39 +1,12 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include<algorithm>	
-#include<map>
-#include<set>
-#include<utility>
-#include<cmath>
-#include<cstring>
-#include<queue>
-#include<stack>
-#include<cstdio>
-#include<sstream>
-#include<iomanip>
+#include<algorithm>
 #define loop(i,a,b) for(int i=a;i<b;i++) 
 #define rep(i,a) loop(i,0,a)
-#define pb push_back
-#define mp make_pair
-#define all(in) in.begin(),in.end()
-#define shosu(x) fixed<<setprecision(x)
 using namespace std;
-//kaewasuretyuui
-typedef long long ll;
-typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef vector<pii> vp;
-typedef vector<vp> vvp;
-typedef vector<string> vs;
-typedef vector<double> vd;
-typedef vector<vd> vvd;
-typedef pair<int,pii> pip;
-typedef vector<pip>vip;
-const double PI=acos(-1);
-const double EPS=1e-7;
-const int inf=1e9;
 int n,k;
 string s;
 vi f(int c){
